b9258d699f10eff3fd5eadea731e885a12389e8c
David_Partsch <117279276+Gommonc@users.noreply.github.com>
2023-02-16 20:37:07 +0100

    test import

--- js/scene.js
+++ js/scene.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import * as THREE from 'three';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // init
 const scene = new THREE.Scene();
